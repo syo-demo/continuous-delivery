367ded799772474b978bd85c4ee3b7e582963de1
Sani Chabi Yo <chabiyosani@gmail.com>
2015-05-14 08:58:17 -0400

    adding rest endpoint

--- src/main/java/com/schabiyo/demo/cd/CdApplication.java
+++ src/main/java/com/schabiyo/demo/cd/CdApplication.java
@@ -2,9 +2,20 @@ package com.schabiyo.demo.cd;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.stereotype.*;
+import org.springframework.boot.*;
 
 @SpringBootApplication
+@RestController
 public class CdApplication {
+
+
+    @RequestMapping("/")
+    @ResponseBody
+    String home(){
+	return "Welcome to a CD demo powered by Cloud Foundry";
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(CdApplication.class, args);
